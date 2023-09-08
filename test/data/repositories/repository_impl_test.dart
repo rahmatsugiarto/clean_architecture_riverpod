fc5857e9840f60afaab54e66dc9821e615dbb078
rahmatsugiarto <sugiartorahmat187@gmail.com>
2023-09-08 14:48:47 +0700

    add case unit test at repository implement

--- test/data/repositories/repository_impl_test.dart
+++ test/data/repositories/repository_impl_test.dart
@@ -1,5 +1,10 @@
+import 'package:clean_architecture_riverpod/common/constants/app_constants.dart';
+import 'package:clean_architecture_riverpod/common/error/failure_response.dart';
+import 'package:clean_architecture_riverpod/common/utils/extension.dart';
 import 'package:clean_architecture_riverpod/data/datasources/remote/remote_datasource.dart';
 import 'package:clean_architecture_riverpod/data/repositories/repository_impl.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
@@ -10,10 +15,12 @@ class MockRemoteDataSource extends Mock implements RemoteDataSource {}
 void main() {
   late MockRemoteDataSource mockRemoteDataSource;
   late RepositoryImpl repository;
+  late DioException dioException;
 
   setUpAll(() {
     mockRemoteDataSource = MockRemoteDataSource();
     repository = RepositoryImpl(remoteDataSource: mockRemoteDataSource);
+    dioException = DioException(requestOptions: RequestOptions());
   });
 
   group("fetchListMovies", () {
@@ -27,6 +34,50 @@ void main() {
       // assert
       final resultList = result.getOrElse(() => []);
       expect(resultList, tListMoviesEntity);
+    });
+
+    test(
+        'should return ConnectionFailure when the device is not connected to the internet',
+        () async {
+      final errorMessage = AppConstants.errorMessage.noInternet;
+      // arrange
+      when(() => mockRemoteDataSource.fetchListMovies())
+          .thenThrow(dioException);
+      // act
+      final result = await repository.fetchListMovies();
+      // assert
+
+      verify(() => mockRemoteDataSource.fetchListMovies());
+      if (dioException.isNoConnectionError) {
+        expect(result, equals(Left(ConnectionFailure(errorMessage))));
+      }
+    });
+
+    test(
+        'should return ServerFailure when it crashes, unless caused by an internet connection error',
+        () async {
+      final errorMessage = AppConstants.errorMessage.noInternet;
+      // arrange
+      when(() => mockRemoteDataSource.fetchListMovies())
+          .thenThrow(dioException);
+      // act
+      final result = await repository.fetchListMovies();
+      // assert
+      verify(() => mockRemoteDataSource.fetchListMovies());
+      if (!dioException.isNoConnectionError) {
+        expect(result, equals(Left(ServerFailure(errorMessage))));
+      }
+    });
+
+    test('should return FailureResponse when all conditions fail', () async {
+      final errorMessage = AppConstants.errorMessage.errorCommon;
+      // arrange
+      when(() => mockRemoteDataSource.fetchListMovies()).thenThrow(Exception());
+      // act
+      final result = await repository.fetchListMovies();
+      // assert
+      verify(() => mockRemoteDataSource.fetchListMovies());
+      expect(result, equals(Left(FailureResponse(errorMessage: errorMessage))));
     });
   });
 }
